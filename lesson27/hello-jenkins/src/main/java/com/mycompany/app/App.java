dec8fb59d4b4211c9d9765866c01870d629f4a9f
ConDoRrus21 <lazareff.d@gmail.com>
2025-12-19 15:29:40 +0300

    change app java file  of hello jenkins app for testiing QG

--- lesson27/hello-jenkins/src/main/java/com/mycompany/app/App.java
+++ lesson27/hello-jenkins/src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,14 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        
+        try {
+    	    int x = 1 / 0;
+        } catch (Exception e) {
+    
+        }
+        
+        System.out.println("Debug message: " + new java.util.Date());
     }
 
     public String getMessage() {
