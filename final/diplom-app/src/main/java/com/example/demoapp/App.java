c7b558f71a0fe202b3603e820bebf9c863f14449
ConDoRrus21 <lazareff.d@gmail.com>
2026-01-17 00:53:59 +0300

    fix name app.java

--- final/diplom-app/src/main/java/com/example/demoapp/App.java
+++ final/diplom-app/src/main/java/com/example/demoapp/App.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoAppApplication {
+public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoAppApplication.class, args);
